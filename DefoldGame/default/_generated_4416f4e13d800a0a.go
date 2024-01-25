9e57d12d7952214401bea94fe41ef919d1623a74
Goribax <goribax@gmail.com>
2024-01-25 10:54:15 -0600

    Defold Game

---
+++ DefoldGame/default/_generated_4416f4e13d800a0a.go
@@ -0,0 +1,17 @@
+components {
+  id: "instrucciones"
+  component: "/main/instrucciones.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  property_decls {
+  }
+}
---
+++ DefoldGame/default/_generated_8d674bc5fb3c0cc7.go
@@ -0,0 +1,65 @@
+components {
+  id: "controller"
+  component: "/main/controller.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  property_decls {
+  }
+}
+embedded_components {
+  id: "menu"
+  type: "collectionproxy"
+  data: "collection: \"/main/main.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "instrucciones"
+  type: "collectionproxy"
+  data: "collection: \"/main/instrucciones.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "juego"
+  type: "collectionproxy"
+  data: "collection: \"/main/juego.collection\"\nexclude: false\n"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
