16f1bc130d2adde26b152cee2186876b6112ae2c
Mau <78014331+Mauerbau@users.noreply.github.com>
2026-01-14 14:40:46 -0600

    prueba15

--- Godot/plataforma/index.service.worker.js
+++ Godot/plataforma/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768421174|6105645215';
+const CACHE_VERSION = '1768423080|8011754130';
 /** @type {string} */
 const CACHE_PREFIX = 'prueba-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
